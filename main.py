028dcbfb1cf3b0881194469faae77e80c4479384
michel <michel.dessenne@gmail.com>
2018-01-24 22:53:15 +0100

    good code

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 # -*- coding: utf-8 -*-
-
-from umqtt.simple import MQTTClient
+import urequests
 import utime
 import ssd1306
 import ure
-import urequests
 from machine import I2C, Pin
 
-def sub_cb(topic, msg):
-    print((topic, msg))
+def pull_aqua():
+    """Import aqua_core-001 json"""
+    url = urequests.get('http://192.168.1.38/json').json()
+    aqua = url['Sensors'][0]['Temperature']
+    return aqua
 
+def pull_xmr_hashrate():
+    """Import XMR json on nanopool"""
+    url = urequests.get('https://api.nanopool.org/v1/xmr/user/46GuwN97BtL2pikZyjb9Aj8fDBAitvNN3St9wW4L1UuNYZn1pYjBUwXWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3ybbnEM').json()
+    hashrate = url['data']['hashrate']
+    return hashrate
+
+def pull_xmr_balance():
+    """Import XMR json on nanopool"""
+    url = urequests.get('https://api.nanopool.org/v1/xmr/user/46GuwN97BtL2pikZyjb9Aj8fDBAitvNN3St9wW4L1UuNYZn1pYjBUwXWbwaVe4vUMveKAzAiA4j8xgUi29TpKXpm3ybbnEM').json()
+    balance = url['data']['balance']
+    return balance
+
+while True:
+    balance = pull_xmr_balance()
+    hashrate = pull_xmr_hashrate()
+    temp = pull_aqua()
 # Affichage oled
     i2c = I2C(sda=Pin(4), scl=Pin(5))
     display = ssd1306.SSD1306_I2C(64, 48, i2c)
     display.fill(0)
-    display.text('{0}'.format(msg), 0, 0)
-    display.text("Je roule", 0, 20)
+    display.text('Tmp:', 0, 0)
+    display.text('{0}'.format(temp), 32, 0)
+    display.text('{0}'.format(balance), 0, 20)
+    display.text('{0}'.format(hashrate), 10, 40)
     display.show()
-
-# Connexion à mosquitto
-c = MQTTClient("umqtt_client", "192.168.1.181",port=1883)
-c.set_callback(sub_cb)
-c.connect()
-c.subscribe("/Aqua-core-001/DHT22/Temperature")
-
-while True:
-    c.check_msg()
-    utime.sleep(0.5)
+    utime.sleep(60)
